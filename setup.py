005485a913e1a5a52eef202ce6e38bff4694cf1c
Sean Hayes <sean@seanhayes.name>
2014-08-08 18:50:51 -0400

    Fixed packages setting in setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import errand_boy
 
 package_name = 'errand_boy'
 test_package_name = 'tests'
 
-setup(name='errand-boy',
+setup(
+    name='errand-boy',
     version=errand_boy.__version__,
-    description="Establish a client/server connection to the errand-boy deamon to execute commands without the memory overhead incurred by os.fork().",
+    description="Establish a connection to the errand-boy deamon to execute commands without the memory overhead incurred by os.fork().",
     author='Seán Hayes',
     author_email='sean@seanhayes.name',
     classifiers=[
@@ -25,9 +26,7 @@ setup(name='errand-boy',
     url='https://github.com/SeanHayes/errand-boy',
     download_url='https://github.com/SeanHayes/errand-boy',
     license='BSD',
-    packages=[
-        package_name,
-    ],
+    packages=find_packages(),
     install_requires=['eventlet'],
     tests_require=[
         'mock',
